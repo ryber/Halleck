0e18e13872ebf040e654687354ab31d576ca61fc
Ryan Bergman <bergmanpatrickr@johndeere.com>
2014-05-16 10:01:13 -0500

    load demo courses from jar

--- src/main/java/halleck/webserver/CourseLoader.java
+++ src/main/java/halleck/webserver/CourseLoader.java
@@ -3,6 +3,7 @@ package halleck.webserver;
 import com.google.common.base.Charsets;
 import com.google.common.base.Strings;
 import com.google.common.io.Files;
+import com.google.common.io.Resources;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import halleck.api.Course;
@@ -13,6 +14,7 @@ import halleck.api.Settings;
 import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Collection;
 import java.util.List;
 
@@ -49,8 +51,22 @@ public class CourseLoader {
 
 
     private String getCourseContent(String courseLoadLocation) {
+        if(courseLoadLocation.startsWith("{rez}")){
+            return getCourseResourceContent(courseLoadLocation);
+        }else {
+            return getCourseContent(new File(courseLoadLocation));
+        }
+    }
 
-        return getCourseContent(new File(courseLoadLocation));
+    private String getCourseResourceContent(String courseLoadLocation) {
+        try {
+            URL url = Resources.getResource(courseLoadLocation.substring(5));
+            return Resources.toString(url, Charsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Course load file not found: " + courseLoadLocation);
+        return null;
     }
 
     private String getCourseContent(File file) {
