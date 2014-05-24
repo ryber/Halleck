81742de1a2673d8391336cad9c57dffe209b0f6a
Ryan Bergman <bergmanpatrickr@johndeere.com>
2014-05-23 19:01:08 -0500

    render them all together now

--- src/main/java/halleck/webserver/renderers/RenderingDispatcher.java
+++ src/main/java/halleck/webserver/renderers/RenderingDispatcher.java
@@ -1,31 +1,37 @@
 package halleck.webserver.renderers;
 
+import com.google.common.collect.Sets;
 import halleck.lms.Course;
 import halleck.webserver.CourseRenderer;
 import halleck.webserver.MustacheRenderer;
 import spark.ModelAndView;
 
+import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class RenderingDispatcher implements CourseRenderer {
 
-    private Function<Course, ModelAndView> linkRenderers;
+    private Set<Function<Course, ModelAndView>> linkRenderers;
 
     private MustacheRenderer mustacheRenderer;
 
     public RenderingDispatcher(){
-        this(new ExternalCourseRenderer(),
-             new MustacheRenderer());
+        this(new MustacheRenderer(),
+             new ExternalCourseRenderer());
     }
 
-    public RenderingDispatcher(Function<Course, ModelAndView> ultLinkRenderer,
-                               MustacheRenderer stach){
-        this.linkRenderers = ultLinkRenderer;
+    public RenderingDispatcher(MustacheRenderer stach,
+                               Function<Course, ModelAndView>... ultLinkRenderer){
+        this.linkRenderers = Sets.newHashSet(ultLinkRenderer);
         this.mustacheRenderer = stach;
     }
 
     @Override
     public String render(Course course) {
-        return mustacheRenderer.render(linkRenderers.apply(course));
+        return linkRenderers.stream()
+                     .map(f -> f.apply(course))
+                     .map(mustacheRenderer::render)
+                     .collect(Collectors.joining("\n\n"));
     }
 }
--- src/test/java/halleck/webserver/RenderingDispatcherTest.java
+++ src/test/java/halleck/webserver/RenderingDispatcherTest.java
@@ -1,15 +1,10 @@
 package halleck.webserver;
 
 import halleck.lms.Course;
-import halleck.webserver.renderers.LinkRenderer;
 import halleck.webserver.renderers.RenderingDispatcher;
-import org.junit.Before;
 import org.junit.Test;
 import spark.ModelAndView;
 
-import java.util.Map;
-
-import static com.google.common.collect.Lists.newArrayList;
 import static org.junit.Assert.assertEquals;
 
 public class RenderingDispatcherTest {
@@ -17,30 +12,25 @@ public class RenderingDispatcherTest {
 
     @Test
     public void willUseFirstPositiveCustomRenderer() throws Exception {
-        MockStacheRenderer stach = new MockStacheRenderer("my result");
-        RenderingDispatcher renderer = new RenderingDispatcher(c -> new ModelAndView(c.getId(), "desert-view.mustache"),stach);
+        RenderingDispatcher renderer = new RenderingDispatcher(
+                new MockStacheRenderer("my result"),
+                c -> new ModelAndView(c.getId(), "a1"),
+                c -> new ModelAndView(c.getId(), "b2"));
 
         String result = renderer.render(new Course("SpiceHarvesterModel"));
 
-        assertEquals("my result", result);
-        assertEquals("SpiceHarvesterModel", stach.data);
-        assertEquals("desert-view.mustache", stach.template);
+        assertEquals("my result\n\nmy result", result);
     }
 
     private static class MockStacheRenderer extends MustacheRenderer {
-        String template;
-        Object data;
         private String result;
 
         public MockStacheRenderer(String result) {
-
             this.result = result;
         }
 
         @Override
         public String renderTemplate(String template, Object data) {
-            this.template = template;
-            this.data = data;
             return result;
         }
     }
